cd56ea53ff15db14aeb1a79fdb17ac26b5e68aab
largelymfs <largelymfs@gmail.com>
2015-04-09 15:11:02 +0800

    Change the structure

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+
+
+app = Flask(__name__)
+app.DEBUG = True
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+if __name__=="__main__":
+    app.run(host = "166.111.5.226")
+
