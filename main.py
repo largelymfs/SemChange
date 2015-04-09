6afe5a26b390aaac3def2f23c87350616130cb07
largelymfs <largelymfs@gmail.com>
2015-04-09 19:30:40 +0800

    Add Post

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 app.DEBUG = True
 
+def search_result_oneword(word):
+    return None
+
 def shutdown_server():
     func = request.environ.get("werkzeug.server.shutdown")
     if func is None:
@@ -21,6 +24,17 @@ def hello():
 def shutdown():
     shutdown_server()
     return 'Server shutting down...'
+
+
+@app.route('/oneword', methods=["POST", "GET"])
+def oneword():
+    if request.method=='POST':
+        print request.form
+        word = request.form['word']
+        result = search_result_oneword(word)
+    else:
+        result = None
+    return render_template("oneword.html")
 
 
 if __name__=="__main__":
